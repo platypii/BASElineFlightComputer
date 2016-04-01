32e44159e38cc49853d356ebc6c48ebfcb97f5c4
Kenny Daniel <platypii@gmail.com>
2016-04-01 00:44:41 -0700

    Fix timeToGround by using AGL

--- app/src/main/java/com/platypii/baseline/data/MyFlightManager.java
+++ app/src/main/java/com/platypii/baseline/data/MyFlightManager.java
@@ -12,10 +12,10 @@ public class MyFlightManager {
     public static LatLng homeLoc;
 
     /**
-     * Computes the estimated time to ground based on current location + velocity
+     * Computes the estimated time to ground based on current altitude and climb
      */
     public static double timeToGround() {
-        double timeToGround = -MyAltimeter.altitude / MyAltimeter.climb;
+        double timeToGround = -MyAltimeter.altitudeAGL() / MyAltimeter.climb;
         if(!Util.isReal(timeToGround) || timeToGround < 0.01 || Math.abs(MyAltimeter.climb) < 0.05 || 24 * 60 * 60 < timeToGround) {
             // return NaN if we don't have an accurate landing location (climbing, very close to ground, very long estimate, etc)
             return Double.NaN;
@@ -25,19 +25,19 @@ public class MyFlightManager {
     }
 
     /**
-     * Computes the estimated landing location based on current location + velocity
+     * Computes the estimated landing location based on current location and velocity
      */
     public static LatLng getLandingLocation() {
         // Compute time to ground
-        double timeToGround = timeToGround();
-        if(Util.isReal(timeToGround)) {
-            final MLocation loc = MyLocationManager.lastLoc;
+        final double timeToGround = timeToGround();
+        if(Util.isReal(timeToGround) && MyLocationManager.lastLoc != null) {
+            final MLocation currentLocation = MyLocationManager.lastLoc;
+
             // Compute horizontal distance traveled at current velocity for timeToGround seconds
-            double groundDistance = timeToGround * loc.groundSpeed();
-            double bearing = loc.bearing();
+            double groundDistance = timeToGround * currentLocation.groundSpeed();
+            double bearing = currentLocation.bearing();
 
             // Compute estimated landing location
-            final MLocation currentLocation = MyLocationManager.lastLoc;
             final LatLng landingLocation = currentLocation.moveDirection(bearing, groundDistance);
 
             // Log.d("FlightManager", currentLocation + " -> " + landingLocation + " (" + groundDistance + "m, " + bearing + "°)");
