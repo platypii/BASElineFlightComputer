385e6e2db4c1facae0af2a1072168ae7f2f87634
Kenny Daniel <platypii@gmail.com>
2016-06-17 00:55:58 -0700

    Fix fatal crash on phones without text-to-speech

--- app/src/main/java/com/platypii/baseline/Services.java
+++ app/src/main/java/com/platypii/baseline/Services.java
@@ -2,17 +2,22 @@ package com.platypii.baseline;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.crash.FirebaseCrash;
 import com.platypii.baseline.altimeter.MyAltimeter;
 import com.platypii.baseline.audible.MyAudible;
 import com.platypii.baseline.bluetooth.BluetoothService;
@@ -85,14 +90,12 @@ public class Services {
             MyAltimeter.start(appContext);
 
             Log.i(TAG, "Checking for text-to-speech data");
-            final Intent checkIntent = new Intent();
-            checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-            activity.startActivityForResult(checkIntent, BaseActivity.MY_TTS_DATA_CHECK_CODE);
+            checkTextToSpeech(activity);
         } else {
             Log.v(TAG, "Services already started");
         }
 
-        // TODO: Upload any unsynced files
+        // If you wanted to automatically upload any unsynced files, this is how:
         // TheCloud.uploadAll();
     }
 
@@ -157,6 +160,27 @@ public class Services {
         if(Util.isReal(home_latitude) && Util.isReal(home_longitude)) {
             // Set home location
             MyFlightManager.homeLoc = new LatLng(home_latitude, home_longitude);
+        }
+    }
+
+    private static void checkTextToSpeech(Activity activity) {
+        final Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+
+        // Check if intent is supported
+        final PackageManager pm = activity.getPackageManager();
+        final ResolveInfo resolveInfo = pm.resolveActivity(checkIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        if(resolveInfo != null) {
+            try {
+                activity.startActivityForResult(checkIntent, BaseActivity.MY_TTS_DATA_CHECK_CODE);
+            } catch(ActivityNotFoundException e) {
+                Log.e("TextToSpeech", "Failed to check for TTS package", e);
+                FirebaseCrash.report(e);
+            }
+        } else {
+            Log.e("TextToSpeech", "TTS package not supported");
+            // Let the user know the audible won't be working
+            Toast.makeText(activity, "Audible error: text-to-speech not available", Toast.LENGTH_LONG).show();
         }
     }
 
