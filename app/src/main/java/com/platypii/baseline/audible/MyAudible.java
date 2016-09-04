e38a74811dcf6f66c15a00021fc9c7abcb35dee1
Kenny Daniel <platypii@gmail.com>
2016-09-04 13:18:08 -0700

    Add GPS signal lost speech

--- app/src/main/java/com/platypii/baseline/audible/MyAudible.java
+++ app/src/main/java/com/platypii/baseline/audible/MyAudible.java
@@ -6,6 +6,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import com.platypii.baseline.Services;
 import com.platypii.baseline.altimeter.MyAltimeter;
+import com.platypii.baseline.data.measurements.MLocation;
 import com.platypii.baseline.util.Convert;
 import com.platypii.baseline.data.MyFlightManager;
 import com.platypii.baseline.util.Util;
@@ -23,7 +24,8 @@ public class MyAudible {
     private static boolean isInitialized = false;
     private static boolean isEnabled = false;
 
-    private static boolean northsouth = true;
+    // Have we spoken "stationary" yet in glide ratio mode?
+    private static boolean stationary = false;
 
     public static void init(Context appContext) {
         Log.i(TAG, "Initializing audible");
@@ -106,6 +108,9 @@ public class MyAudible {
         return Float.parseFloat(prefs.getString("audible_rate", "1.0"));
     }
 
+    /**
+     * Returns the text of what to say for the current measurement mode
+     */
     private static String getMeasurement() {
         final String audibleMode = prefs.getString("audible_mode", "horizontal_speed");
         final double units = Convert.metric? Convert.KPH : Convert.MPH;
@@ -116,7 +121,7 @@ public class MyAudible {
             // TODO: True Air Speed
             case "total_speed":
                 // Compute total speed
-                if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 5000) {
+                if(goodGpsFix()) {
                     final double verticalSpeed = MyAltimeter.climb;
                     final double horizontalSpeed = Services.location.lastLoc.groundSpeed();
                     final double totalSpeed = Math.sqrt(verticalSpeed * verticalSpeed + horizontalSpeed * horizontalSpeed);
@@ -125,21 +130,17 @@ public class MyAudible {
                     } else {
                         Log.w(TAG, "Not speaking: total speed = " + Convert.speed(totalSpeed, 0, true));
                     }
-                } else {
-                    Log.w(TAG, "Not speaking: no gps signal");
                 }
                 break;
             case "horizontal_speed":
                 // Read horizontal speed
-                if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 5000) {
+                if(goodGpsFix()) {
                     final double horizontalSpeed = Services.location.lastLoc.groundSpeed();
                     if (Util.isReal(horizontalSpeed) && min * units <= horizontalSpeed && horizontalSpeed <= max * units) {
                         measurement = Convert.speed(horizontalSpeed, 0, false);
                     } else {
                         Log.w(TAG, "Not speaking: horizontal speed = " + Convert.speed(horizontalSpeed, 0, true));
                     }
-                } else {
-                    Log.w(TAG, "Not speaking: no gps signal");
                 }
                 break;
             case "vertical_speed":
@@ -157,62 +158,43 @@ public class MyAudible {
                 break;
             case "glide_ratio":
                 // Read glide ratio
-                if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 5000) {
-                    final double glideRatio = Services.location.lastLoc.glideRatio();
+                if(goodGpsFix()) {
+                    final MLocation loc = Services.location.lastLoc;
+                    final double glideRatio = loc.glideRatio();
                     if(Util.isReal(glideRatio) && min <= glideRatio && glideRatio <= max) {
-                        measurement = Convert.glide(glideRatio, 1, false);
+                        // If horizontal or vertical speed less than .2mph:
+                        if (loc.groundSpeed() < 0.1) {
+                            if (Math.abs(loc.climb) < 0.1) {
+                                if (!stationary) {
+                                    stationary = true;
+                                    measurement = "Stationary";
+                                } else {
+                                    measurement = "";
+                                }
+                            } else {
+                                stationary = false;
+                                measurement = "Vertical";
+                            }
+                        } else {
+                            stationary = false;
+                            measurement = Convert.glide(glideRatio, 1, false);
+                        }
                     } else {
                         Log.w(TAG, "Not speaking: glide ratio = " + Convert.glide(glideRatio, 1, true));
                     }
-                } else {
-                    Log.w(TAG, "Not speaking: no gps signal");
                 }
                 break;
             case "direction":
-                if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 5000) {
-                    if(MyFlightManager.homeLoc != null) {
-                        final double distance = Services.location.lastLoc.distanceTo(MyFlightManager.homeLoc);
-                        if(min <= distance && distance <= max) {
-                            final double bearing = Services.location.lastLoc.bearingTo(MyFlightManager.homeLoc);
-                            if (Math.abs(distance) > 0.3) {
-                                measurement = Convert.distance(distance) + " " + Convert.bearing(bearing);
-                            } else {
-                                measurement = "0";
-                            }
-                        }
-                    }
-                } else {
-                    Log.w(TAG, "Not speaking: no gps signal");
-                }
-                break;
-            case "position":
-                // Read position N10E45
-                if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 5000) {
-                    if(MyFlightManager.homeLoc != null) {
+                if(goodGpsFix() && MyFlightManager.homeLoc != null) {
+                    final double distance = Services.location.lastLoc.distanceTo(MyFlightManager.homeLoc);
+                    if(min <= distance && distance <= max) {
                         final double bearing = Services.location.lastLoc.bearingTo(MyFlightManager.homeLoc);
-                        final double distance = Services.location.lastLoc.distanceTo(MyFlightManager.homeLoc);
-                        final double distance_x = distance * Math.sin(Math.toRadians(bearing));
-                        final double distance_y = distance * Math.cos(Math.toRadians(bearing));
-                        if(northsouth && Math.abs(distance_y) > 0.3) {
-                            if (-90 < bearing && bearing < 90) {
-                                measurement = "north " + Convert.distance(distance_y, 0, false);
-                            } else {
-                                measurement = "south " + Convert.distance(distance_y, 0, false);
-                            }
-                            northsouth = false;
-                        } else if(Math.abs(distance_x) > 0.3) {
-                            if(0 < bearing) {
-                                measurement += "east " + Convert.distance(distance_x, 0, false);
-                            } else {
-                                measurement += "west " + Convert.distance(distance_x, 0, false);
-                            }
-                            northsouth = true;
+                        if (Math.abs(distance) > 0.3) {
+                            measurement = Convert.distance(distance) + " " + Convert.bearing(bearing);
                         } else {
                             measurement = "0";
                         }
                     }
-                } else {
-                    Log.w(TAG, "Not speaking: no gps signal");
                 }
                 break;
             default:
@@ -225,9 +207,23 @@ public class MyAudible {
         return isEnabled;
     }
 
-//    public static boolean isRunning() {
-//        return isInitialized && audibleThread != null && audibleThread.isRunning();
-//    }
+    /**
+     * Return true if GPS signal is fresh
+     */
+    private static boolean goodGpsFix() {
+        if(Services.location.lastLoc != null && Services.location.lastFixDuration() < 3500) {
+            gpsFix = true;
+        } else {
+            Log.w(TAG, "Stale GPS signal");
+            if(gpsFix) {
+                speech.speakNow("Signal lost");
+            }
+            gpsFix = false;
+        }
+        return gpsFix;
+    }
+    /** True iff the last measurement was a good fix */
+    private static boolean gpsFix = false;
 
     /**
      * Stop audible service
