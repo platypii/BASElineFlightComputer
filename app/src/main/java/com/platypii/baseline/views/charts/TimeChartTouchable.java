5b03b6a7f064fccef22c9becba574abafe64f29b
Kenny Daniel <platypii@gmail.com>
2018-03-15 17:35:31 -0700

    Fix null pointer in time chart

--- app/src/main/java/com/platypii/baseline/views/charts/TimeChartTouchable.java
+++ app/src/main/java/com/platypii/baseline/views/charts/TimeChartTouchable.java
@@ -3,6 +3,7 @@ package com.platypii.baseline.views.charts;
 import com.platypii.baseline.events.ChartFocusEvent;
 import com.platypii.baseline.measurements.MLocation;
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import java.util.Collections;
@@ -32,12 +33,17 @@ public class TimeChartTouchable extends TimeChart {
     /**
      * Performs a binary search for the nearest data point
      */
+    @Nullable
     private MLocation findClosest(long millis) {
-        touchLocation.millis = millis;
-        int closest_index = Collections.binarySearch(trackData, touchLocation);
-        if (closest_index < 0) closest_index = -closest_index;
-        if (closest_index == trackData.size()) closest_index--;
-        return trackData.get(closest_index);
+        if (trackData != null && !trackData.isEmpty()) {
+            touchLocation.millis = millis;
+            int closest_index = Collections.binarySearch(trackData, touchLocation);
+            if (closest_index < 0) closest_index = -closest_index;
+            if (closest_index == trackData.size()) closest_index--;
+            return trackData.get(closest_index);
+        } else {
+            return null;
+        }
     }
 
 }
