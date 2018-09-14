b937373b83c4b5a1820c8ea59ce779c6c9d2ea1c
Kenny Daniel <platypii@gmail.com>
2018-09-14 16:42:27 -0700

    Migrate AuthToken to new GoogleSignInClient

--- app/src/main/java/com/platypii/baseline/cloud/AuthException.java
+++ app/src/main/java/com/platypii/baseline/cloud/AuthException.java
@@ -6,4 +6,7 @@ public class AuthException extends IOException {
     public AuthException(String auth) {
         super("authorization error - auth: " + auth);
     }
+    AuthException(String auth, Exception e) {
+        super("authorization error - auth: " + auth, e);
+    }
 }
--- app/src/main/java/com/platypii/baseline/cloud/AuthToken.java
+++ app/src/main/java/com/platypii/baseline/cloud/AuthToken.java
@@ -4,12 +4,13 @@ import com.platypii.baseline.R;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
-import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Tools for getting google sign in authentication tokens
@@ -22,32 +23,29 @@ public class AuthToken {
      */
     public static @NonNull String getAuthToken(@NonNull Context context) throws AuthException {
         final long startTime = System.currentTimeMillis();
-        final GoogleApiClient googleApiClient = getClient(context);
+        final GoogleSignInClient signInClient = getClient(context);
+        final Task<GoogleSignInAccount> futureAccount = signInClient.silentSignIn();
         try {
-            final ConnectionResult result = googleApiClient.blockingConnect();
-            if (result.isSuccess()) {
-                final GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.silentSignIn(googleApiClient).await();
-                final GoogleSignInAccount account = googleSignInResult.getSignInAccount();
-                if (account != null) {
-                    final String authToken = account.getIdToken();
-                    if (authToken != null) {
-                        Log.i(TAG, "Got auth token in " + (System.currentTimeMillis() - startTime) + " ms");
-                        return authToken;
-                    } else {
-                        throw new AuthException("Failed to get auth token");
-                    }
+            final GoogleSignInAccount account = Tasks.await(futureAccount);
+            if (account != null) {
+                final String authToken = account.getIdToken();
+                if (authToken != null) {
+                    Log.i(TAG, "Got auth token in " + (System.currentTimeMillis() - startTime) + " ms");
+                    return authToken;
                 } else {
-                    throw new AuthException("Not signed in");
+                    throw new AuthException("Failed to get auth token");
                 }
             } else {
-                throw new AuthException("Sign in connection error");
+                throw new AuthException("Not signed in");
             }
-        } finally {
-            googleApiClient.disconnect();
+        } catch (ExecutionException e) {
+            throw new AuthException("Sign in failed", e);
+        } catch (InterruptedException e) {
+            throw new AuthException("Sign in interrupted", e);
         }
     }
 
-    private static GoogleApiClient getClient(@NonNull Context context) {
+    private static GoogleSignInClient getClient(@NonNull Context context) {
         // Prepare google sign options
         final String serverClientId = context.getString(R.string.server_client_id);
         final GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -55,10 +53,8 @@ public class AuthToken {
                 .requestEmail()
                 .build();
 
-        // Build a GoogleApiClient with access to the Google Sign-In API
-        return new GoogleApiClient.Builder(context)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+        // Build a GoogleSignInClient with access to the Google Sign-In API
+        return GoogleSignIn.getClient(context, gso);
     }
 
 }
