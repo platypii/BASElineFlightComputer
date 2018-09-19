606c22e5a04a785fe89083e10a44e96d1b8a0423
Kenny Daniel <platypii@gmail.com>
2018-09-18 19:41:55 -0700

    Handle missing samples in RefreshRateEstimator

--- common/src/main/java/com/platypii/baseline/util/RefreshRateEstimator.java
+++ common/src/main/java/com/platypii/baseline/util/RefreshRateEstimator.java
@@ -1,23 +1,34 @@
 package com.platypii.baseline.util;
 
 public class RefreshRateEstimator {
+    private static final float epsilon = 0.05f;
 
     public float refreshRate = 0;
     private long lastMillis = -1;
+
+    // How many misses in a row
+    private int missedCount = 0;
 
     public void addSample(long millis) {
         final long delta = millis - lastMillis; // time since last refresh
         // Deltas over 62 seconds are discarded (so we can handle 1 sample/min)
         if (0 <= lastMillis && 0 < delta && delta < 62000L) {
             final float newRefreshRate = 1000f / delta; // Refresh rate based on last 2 samples
+            // Special case for missing sample where refreshRate ~= 2 * newRefreshRate
+            final boolean missed = 2 * newRefreshRate - epsilon < refreshRate && refreshRate < 2 * newRefreshRate + epsilon;
             if (refreshRate == 0) {
                 refreshRate = newRefreshRate;
+            } else if (missed && missedCount == 0) {
+                // Special case for first missing sample
+                missedCount++;
             } else {
-                refreshRate += (newRefreshRate - refreshRate) * 0.5f; // Moving average
+                missedCount = 0;
+                // Moving average
+                refreshRate += (newRefreshRate - refreshRate) * 0.5f;
             }
 
             // Sanity checks
-            if (Double.isNaN(refreshRate) || Double.isInfinite(refreshRate)) {
+            if (refreshRate < 0 || Double.isNaN(refreshRate) || Double.isInfinite(refreshRate)) {
                 Exceptions.report(new Exception("Invalid refresh rate, delta = " + delta + " refreshRate = " + refreshRate));
                 refreshRate = 0;
             }
--- common/src/test/java/com/platypii/baseline/util/RefreshRateEstimatorTest.java
+++ common/src/test/java/com/platypii/baseline/util/RefreshRateEstimatorTest.java
@@ -43,4 +43,39 @@ public class RefreshRateEstimatorTest {
         assertEquals(5.0, estimator.refreshRate, 0.01);
     }
 
+    @Test
+    public void missingSample() {
+        RefreshRateEstimator estimator = new RefreshRateEstimator();
+        estimator.addSample(0);
+        estimator.addSample(200);
+        estimator.addSample(400);
+        assertEquals(5.0, estimator.refreshRate, 0.01);
+        // estimator.addSample(600);
+        estimator.addSample(800);
+        assertEquals(5.0, estimator.refreshRate, 0.01);
+    }
+
+    @Test
+    public void halfRefreshRate() {
+        // Drop from 2hz to 1hz
+        RefreshRateEstimator estimator = new RefreshRateEstimator();
+        estimator.addSample(0);
+        estimator.addSample(500);
+        estimator.addSample(1000);
+        assertEquals(2.0, estimator.refreshRate, 0.01);
+        estimator.addSample(2000);
+        assertEquals(2.0, estimator.refreshRate, 0.01); // assumes missing sample
+        estimator.addSample(3000);
+        assertEquals(1.0, estimator.refreshRate, 0.5); // starts adjusting
+        estimator.addSample(4000);
+        assertEquals(1.0, estimator.refreshRate, 0.25);
+        estimator.addSample(5000);
+        estimator.addSample(6000);
+        assertEquals(1.0, estimator.refreshRate, 0.1);
+        estimator.addSample(7000);
+        estimator.addSample(8000);
+        estimator.addSample(9000);
+        assertEquals(1.0, estimator.refreshRate, 0.01); // close enough
+    }
+
 }
