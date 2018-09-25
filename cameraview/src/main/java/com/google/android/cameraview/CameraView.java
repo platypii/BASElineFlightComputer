57aa8d00c3855335c4c4561f980392af228ad50f
Kenny Daniel <platypii@gmail.com>
2018-09-24 22:45:44 -0700

    Simplify cameraview by limiting to sdk 14+

--- cameraview/src/main/java/com/google/android/cameraview/CameraView.java
+++ cameraview/src/main/java/com/google/android/cameraview/CameraView.java
@@ -123,13 +123,7 @@ public class CameraView extends FrameLayout {
 
     @NonNull
     private PreviewImpl createPreviewImpl(Context context) {
-        PreviewImpl preview;
-        if (Build.VERSION.SDK_INT < 14) {
-            preview = new SurfaceViewPreview(context, this);
-        } else {
-            preview = new TextureViewPreview(context, this);
-        }
-        return preview;
+        return new TextureViewPreview(context, this);
     }
 
     @Override
