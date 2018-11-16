ac58bf1080c47336b805d7c97eeec15e077cc12d
Kenny Daniel <platypii@gmail.com>
2018-11-15 21:51:15 -0800

    Add BASE icons

--- app/src/main/java/com/platypii/baseline/views/map/PlaceIcons.java
+++ app/src/main/java/com/platypii/baseline/views/map/PlaceIcons.java
@@ -7,11 +7,11 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 public class PlaceIcons {
 
-    private static final BitmapDescriptor b = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
-    private static final BitmapDescriptor a = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
-    private static final BitmapDescriptor s = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
-    private static final BitmapDescriptor e = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
-    private static final BitmapDescriptor dz = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
+    private static final BitmapDescriptor b = BitmapDescriptorFactory.fromResource(R.drawable.shadow_b);
+    private static final BitmapDescriptor a = BitmapDescriptorFactory.fromResource(R.drawable.shadow_a);
+    private static final BitmapDescriptor s = BitmapDescriptorFactory.fromResource(R.drawable.shadow_s);
+    private static final BitmapDescriptor e = BitmapDescriptorFactory.fromResource(R.drawable.shadow_e);
+    private static final BitmapDescriptor dz = BitmapDescriptorFactory.fromResource(R.drawable.shadow_dz);
     private static final BitmapDescriptor other = BitmapDescriptorFactory.fromResource(R.drawable.ic_pin);
 
     @NonNull
